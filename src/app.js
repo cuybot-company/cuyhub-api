88eb3dd4f722410016c912268fb2a1e597ec2111
dea <dea.aprizal@icloud.com>
2021-10-20 19:46:41 +0700

    fix home

--- src/app.js
+++ src/app.js
@@ -23,14 +23,19 @@ app.use(express.static(__dirname + 'public'))
 const main_routes = require('./routes/index')
 const guild = require('./routes/guild/guildRoutes')
 
+
 main_routes(app)
+
+app.get('/', function (req, res) {
+  res.send('Cuyhub API V1 is ready to use!')
+})
 
 app.use((req, res, next) => {
     const bearerToken = token
     if (
       req.headers.authorization &&
       req.headers.authorization.split(' ')[0] == 'Bearer'
-    ) {
+      ) {
       var setBearer = req.headers.authorization.split(' ')[1]
       if (setBearer != bearerToken) {
         return response.errAuthorize('UnAuthorized Permission Denied', res.status(500))
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,5 @@
 const Controller = require('../controllers/mainController')
 
 module.exports = function (app) {
-  app.route('/').get(Controller.index)
   app.route('/v1').get(Controller.main)
 }
